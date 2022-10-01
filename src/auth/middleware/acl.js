952e445a5688341666de5407c0270954c0941115
AllieDunkel <alliedunkel@outlook.com>
2022-10-01 13:21:37 -0500

    done with acl.js file

--- src/auth/middleware/acl.js
+++ src/auth/middleware/acl.js
@@ -1,1 +1,14 @@
 'use strict';
+
+models.exports = (capability) => (req, res, next) => {
+  try {
+    console.log('from acl middleware', req.user);
+    if(req.user.capabilities.includes(capability)) {
+      next();
+    } else {
+      next('Access Denied');
+    }
+  } catch(e) {
+    next('invalid ACL Login');
+  }
+};
